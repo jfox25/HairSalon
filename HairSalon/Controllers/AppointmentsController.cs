206284a5f9e5922e8f18e71edbf63f57508d3242
James Fox <jimmyfox00@gmail.com>
2022-05-20 12:21:55 -0700

    WIP: lunch break

--- HairSalon/Controllers/AppointmentsController.cs
+++ HairSalon/Controllers/AppointmentsController.cs
@@ -1,10 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HairSalon.Controllers
 {
     public class AppointmentsController : Controller
     {
+      private readonly HairSalonContext _db;
 
+      public AppointmentsController(HairSalonContext db)
+      {
+        _db = db;
+      }
+      public ActionResult Index()
+      {
+        List<Appointment> appointments = _db.Appointments.ToList();
+        ViewBag.StylistCount = _db.Stylists.ToList().Count;
+        ViewBag.ClientsCount = _db.Clients.ToList().Count;
+        return View(appointments);
+      }
 
     }
 }
