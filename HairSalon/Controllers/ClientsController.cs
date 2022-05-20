63a09d26b2acb8ba897b043d5091c0e2539fcc5d
James Fox <jimmyfox00@gmail.com>
2022-05-20 11:17:19 -0700

    Add full cruf functionality to clients

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -25,17 +25,22 @@ namespace HairSalon.Controllers
       public ActionResult Create(int id)
       {
         ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+        if(id != 0)
+        {
+          Stylist stylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+          ViewBag.StylistName = stylist.Name;
+        }
         ViewBag.fromStylistId = id;
         return View();
       }
       [HttpPost]
-      public ActionResult Create(Client client, bool returnToStylist = false)
+      public ActionResult Create(Client client, string returnToStylist = "false")
       {
         _db.Clients.Add(client);
         _db.SaveChanges();
-        if(returnToStylist == true)
+        if(returnToStylist == "true")
         {
-          return Redirect($"/stylists/{client.StylistId}");
+          return Redirect($"/stylists/details/{client.StylistId}");
         }
         return RedirectToAction("Index");
       }
@@ -45,6 +50,27 @@ namespace HairSalon.Controllers
         Stylist stylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == client.StylistId);
         ViewBag.StylistName = stylist.Name;
         return View(client);
+      }
+       public ActionResult Edit(int id)
+      {
+        Client client = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+        ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+        return View(client);
+      }
+      [HttpPost]
+      public ActionResult Edit(Client client)
+      {
+        _db.Entry(client).State = EntityState.Modified;
+        _db.SaveChanges();
+        return Redirect($"/clients/details/{client.ClientId}");
+      }
+      [HttpPost]
+      public ActionResult Delete(int id)
+      {
+        Client client = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+        _db.Clients.Remove(client);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
       }
 
     }
